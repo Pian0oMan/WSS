f4dcc01f37ca1aa142a922c13bbd54e2cda37abd
jcb632 <jcbanuelos@cpp.edu>
2026-04-28 13:47:43 -0700

    Add files via upload

---
+++ Brain.java
@@ -0,0 +1,43 @@
+public class Brain{
+
+	private Player player;
+	private String brainType;
+	private Vision vision;
+
+	public Brain(Player p, Map m, String brainType, String visionType){
+		player = p;
+		this.brainType = brainType;
+		vision = new Vision(this, m, visionType);
+	}
+
+	public void makeMove(){
+		vision.look();
+		//figure out how to have different brainTypes decide differently (east then food vs food then east)
+	}
+
+	public String[] decisionList(){
+		if(brainType.equals("A"){
+			return new String[] { "East", "Survival", "Trader" };
+		}
+		else if(brainType.equals("B"){
+			return new String[] { "East", "Survival", "Trader" };
+		}
+		else if(brainType.equals("C"){
+			return new String[] { "East", "Survival", "Trader" };
+		}
+		else if(brainType.equals("D"){
+			return new String[] { "East", "Survival", "Trader" };
+		}
+		else{
+			return new String[] { "East", "Survival", "Trader" };
+		}
+	}
+	}
+
+	public int getRowPos(){
+		return player.getRowPos();
+	}
+	public int getColPos(){
+		return player.getColPos();
+	}
+}
---
+++ Vision.java
@@ -0,0 +1,65 @@
+public class Vision{
+
+	private Brain brain;
+	private Map map;
+	private int[][] sight;
+	private Square[] tiles;
+
+	public Vision(Brain b, Map m, String visionType){
+		brain = b;
+		map = m;
+		if(visionType.equals("Focused"){
+			sight = new int[][] { {-1, 1}, {0, 1}, {1, 1}, {-1, 2}, {0, 2}, {1, 2} };
+		}
+		else if(visionType.equals("Eyes Peeled"){
+			sight = new int[][] { {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}, {0, 2} };
+		}
+		else if(visionType.equals("Far Sighted"){
+			sight = new int[][] { {0, 1}, {-1, 2}, {0, 2}, {1, 2}, {-2, 3}, {2, 3} };
+		}
+		else if(visionType.equals("Cross Eyed"){
+			sight = new int[][] { {-1, 0}, {1, 0}, {-2, 1}, {-1, 1}, {1, 1}, {2, 1} };
+		}
+		else{
+			sight = new int[][] { {0, 1} };
+		}
+	}
+
+	public void look(){
+		tiles = new Square[sight.length];
+		for(int i = 0; i < sight.length; i++){
+			tiles[i] = map.getMapSquare(brain.getXPos() + sight[i][0], brain.getYPos() + sight[i][1]);
+		}
+	}
+
+	public Path findFood(){
+		int best = -1;
+		for(int i = 0; i < tiles.length; i++){
+			Square s = tiles[i];
+			if(s.getItem() != null){
+				if(best == -1){
+					best = s;
+				}
+				else{
+					if(best.)
+				}
+			}
+		}
+		if(best == -1){
+			return null;
+		}
+		return new Path(sight[i], tiles[i].getTerrain().getCosts());
+	}
+
+	public Path findWater(){
+
+	}
+
+	public Path findTrader(){
+
+	}
+
+	public Path findEast(){
+
+	}
+}
